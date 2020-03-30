b474ca6a0e01d6ea32e5f4a707e98cffc77c8d80
JordanHardie <62230359+JordanHardie@users.noreply.github.com>
2020-03-30 16:51:15 +1100

    Added comments 2: Electric boogaloo

--- A2_Jordan_Hardie/Assets/Scripts/GemScript.cs
+++ A2_Jordan_Hardie/Assets/Scripts/GemScript.cs
@@ -6,14 +6,19 @@ public class GemScript : MonoBehaviour
 {
     void Update()
     {
+        //Frame rate independant spinning animation.
         transform.Rotate(20 * Time.deltaTime, 20 * Time.deltaTime, 20 * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        //If the gem runs into the player.
         if(collision.collider.gameObject.name == "Player")
         {
+            //Why didn't I just put the incGem function in the player movement script?
+            //The amount of gem's the player has increases the gun fire rate.
             collision.collider.GetComponentInChildren<GunScript>().incGem();
+            //Destroy the gem.
             Destroy(gameObject);
         }
     }
